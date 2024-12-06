e7dd12aefc608dc58d3b8149a7475a3c990cb1b5
LuisBBandeira <luisfilipebentobandeira@gmail.com>
2024-12-06 21:51:52 +0000

    aula3

---
+++ exercicios_aula_3/socket_app/server/main.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = "0.0.0.0"
+PORT = 545  
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT)) 
+    s.listen()  
+    print(f"Server listening on {HOST}:{PORT}")
+    
+    conn, addr = s.accept()  
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)  
+            if not data:  
+                break
+            conn.sendall(data) 
